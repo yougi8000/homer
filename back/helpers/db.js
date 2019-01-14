b9ee9da50dda1d8416864d694b6f2bcd9d1a5c32
DE RUIJTER Michaël <42461239+yougi8000@users.noreply.github.com>
2019-01-14 15:16:23 +0100

    Update db.js

--- back/helpers/db.js
+++ back/helpers/db.js
@@ -1,7 +1,7 @@
 const mysql      = require('mysql');
 const connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'wilder',
+  user     : '*********',
   password : '*********',
   database : 'homer'
 });
